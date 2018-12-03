d591708bc85a8f388d528565970e789e822d4197
Simon vLac <44780442+SimonvLac@users.noreply.github.com>
2018-12-03 12:38:55 -0500

    Add files via upload

---
+++ template.py
@@ -0,0 +1,28 @@
+"""
+Created on %(date)s
+
+@author: %(username)s
+
+Project: FuturaeNetcom/4chan
+"""
+from z import x
+
+
+str_FILE_NAME = 'file.txt'
+
+
+# main part of the programm
+def main():
+    return
+
+
+# used as an example.
+def example_function(str_newImport, num_exampleNumber):
+    try:
+        pass
+    except OverflowError:
+        pass
+    return
+
+
+main()
